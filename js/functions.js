99d2e6cac6cacbd6f81469d9f01ed1b4b95df314
Ганина Анастасия <122857367+AnastasiyaGanina@users.noreply.github.com>
2023-06-14 21:17:46 +0300

    correction

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,16 @@
 function getStringLength (string, length) {
-  return string.length > length ? 'true' : 'false';
+  return string.length > length;
 }
 window.console.log(getStringLength ('попробовать', 12));
 
-function checkString (string) {
+function isPolindrome (string) {
   const normalizedString = string.replaceAll(' ', '').toLowerCase();
-
   let newString = '';
 
   for (let i = normalizedString.length - 1; i >= 0; --i) {
     newString += normalizedString[i];
   }
-  return newString === normalizedString ? 'true' : 'false';
+  return newString === normalizedString;
 }
 
-window.console.log(checkString ('Я иду с      мечем судия'));
+window.console.log(isPolindrome ('Я иду с      мечем судия'));
