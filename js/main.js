ee22588432b36143a78aeaec1b75d17bdc7e57a5
Ганина Анастасия <122857367+AnastasiyaGanina@users.noreply.github.com>
2023-07-04 19:32:31 +0300

    something

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ import {renderPictures} from './pictures.js';
 import {getPhotosData} from './data.js';
 import './user-modal.js';
 
-window.console.log(getPhotosData(25));
+// window.console.log(getPhotosData(25));
 
 renderPictures(getPhotosData(25));
--- js/user-modal.js
+++ js/user-modal.js
@@ -4,19 +4,19 @@ import {getPhotosData} from './data.js';
 
 const bigPictureModal = document.querySelector('.big-picture');
 const picturesContainer = document.querySelector('.pictures');
-const bigPictureOpen = document.querySelectorAll('.picture');
+const bigPictureOpen = Array.from(document.querySelectorAll('.picture'));
 const bigPictureClose = bigPictureModal.querySelector('.big-picture__cancel');
 
 const commentsCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
 
-const picture = document.querySelector('.big-picture__img img');
+const pictureBig = document.querySelector('.big-picture__img');
 const likes = document.querySelector('.likes-count');
 const comments = document.querySelector('.comments-count');
 const socialComments = document.querySelector('.social__comments');
 const socialComment = document.querySelector('.social__comment');
 const socialCaption = document.querySelector('.social__caption');
-const picturesArray = getPhotosData(25);
+
 
 const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -25,27 +25,39 @@ const onDocumentKeydown = (evt) => {
   }
 };
 
-function onClick (evt) {
+const onClick = (evt) => {
   if (evt.target.closest('.picture')){
     bigPictureModal.classList.remove('hidden');
     document.querySelector('body').classList.add('modal-open');
     commentsCount.classList.add('hidden');
     commentsLoader.classList.add('hidden');
+    // console.log(evt.target.closest('.picture').dataset.id);
+    for (let i = 0; i < bigPictureOpen.length; i++) {
+      // if (evt.target.closest('.picture').dataset.id === bigPictureOpen[i].id.toString()) {
+      pictureBig.src = bigPictureOpen[i].url;
+      likes.textContent = bigPictureOpen[i].likes;
+      comments.textContent = bigPictureOpen[i].comments.length;
+      socialCaption.textContent = bigPictureOpen[i].description;
+      // }
 
-    for (let i = 0; i < picturesArray.length; i++) {
-      picture.src = picturesArray[i].url;
-      likes.textContent = picturesArray[i].likes;
-      comments.textContent = picturesArray[i].comments.length;
-      socialCaption.textContent = picturesArray[i].description;
+      for (let j = 0; j < bigPictureOpen[i].comments.length; j++) {
+        const element = socialComment.cloneNode(true);
+        element.querySelector('.social__picture').src = bigPictureOpen[i].comments[j].avatar;
+        element.querySelector('.social__picture').alt = bigPictureOpen[i].comments[j].name;
+        element.querySelector('.social__text').textContent = bigPictureOpen[i].comments[j].message;
+        socialComments.appendChild(element);
+      }
     }
   }
-}
+};
+
 
 picturesContainer.addEventListener('click', onClick);
 document.addEventListener('keydown', onDocumentKeydown);
 
 bigPictureClose.addEventListener('click', () => {
   bigPictureModal.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
 
   document.removeEventListener('keydown', onDocumentKeydown);
 });
