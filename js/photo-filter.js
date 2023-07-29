0dae208cf4acc8e3d2c353519fd1ed8d47625db4
Ганина Анастасия <122857367+AnastasiyaGanina@users.noreply.github.com>
2023-07-29 19:59:47 +0300

    corrects an error in photo filtering

--- js/photo-filter.js
+++ js/photo-filter.js
@@ -1,63 +1,53 @@
 import { dataPhotos } from './load.js';
-import {renderPictures } from './pictures.js';
-import {debounce} from './util.js';
+import {renderPicturesWithDebounce} from './pictures.js';
+
 
 const filterDefaultButten = document.querySelector('#filter-default');
 const filterRandomButten = document.querySelector('#filter-random');
 const filterDiscussedButten = document.querySelector('#filter-discussed');
-const picturesContainer = document.querySelector('.pictures');
-
-function resetPhotos() {
-  const pictures = picturesContainer.querySelectorAll('.picture');
-  pictures.forEach((picture)=> {
-    picture.remove();
-  });
-}
-
-function addActiveClass (buttonA, buttonB, buttonActive) {
-  buttonA.classList.remove('img-filters__button--active');
-  buttonB.classList.remove('img-filters__button--active');
-  buttonActive.classList.add('img-filters__button--active');
-}
+const imageFilter = document.querySelector('.img-filters');
+const imageFilterButton = imageFilter.querySelector('.img-filters__form');
 
 function getRandomPhotos(arr) {
-  const duplicate = arr.slice();
-  for (let i = 0 ; (i < 10) && (i < duplicate.length) ; i++) {
-    const r = Math.floor(Math.random() * (duplicate.length - i)) + i;
-    const photo = duplicate[r];
-    duplicate[r] = duplicate[i];
-    duplicate[i] = photo;
+  for (let i = 0 ; (i < 10) && (i < arr.length) ; i++) {
+    const r = Math.floor(Math.random() * (arr.length - i)) + i;
+    const photo = arr[r];
+    arr[r] = arr[i];
+    arr[i] = photo;
   }
-  return duplicate.slice(0, 10);
+  return arr.slice(0, 10);
 }
 
 function getDiscussedPhotosFirst (arr) {
-  const duplicate = arr.slice();
-  duplicate.sort((a, b) => b.comments.length - a.comments.length);
-  return duplicate;
+  return arr.sort((a, b) => b.comments.length - a.comments.length);
 }
 
-function onfilterRandomButtenClick() {
-  addActiveClass (filterDefaultButten, filterDiscussedButten, filterRandomButten);
-  resetPhotos();
-
-  renderPictures(getRandomPhotos(dataPhotos));
+function getFilterData (id) {
+  const idToFilter = {
+    'filter-default': dataPhotos,
+    'filter-random': getRandomPhotos([...dataPhotos]),
+    'filter-discussed': getDiscussedPhotosFirst ([...dataPhotos])
+  };
+  return idToFilter[id];
 }
 
-function onfilterDiscussedButtenClick() {
-  addActiveClass (filterRandomButten, filterDefaultButten, filterDiscussedButten);
-  resetPhotos();
-
-  renderPictures(getDiscussedPhotosFirst (dataPhotos));
+function setActiveFilterButton (evt) {
+  imageFilterButton.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+  evt.target.classList.add('img-filters__button--active');
 }
 
-function onfilterDefaultButtenClick() {
-  resetPhotos();
-  addActiveClass (filterRandomButten, filterDiscussedButten, filterDefaultButten);
-
-  renderPictures(dataPhotos);
+function onFilterClick(evt) {
+  const pictures = getFilterData (evt.target.id);
+  setActiveFilterButton(evt);
+  renderPicturesWithDebounce(pictures);
 }
 
-filterDefaultButten.addEventListener('click', debounce(onfilterDefaultButtenClick));
-filterRandomButten.addEventListener('click', debounce(onfilterRandomButtenClick));
-filterDiscussedButten.addEventListener('click', debounce(onfilterDiscussedButtenClick));
+function initializeFilters() {
+  imageFilter.classList.remove('img-filters--inactive');
+
+  filterDefaultButten.addEventListener('click', onFilterClick);
+  filterRandomButten.addEventListener('click', onFilterClick);
+  filterDiscussedButten.addEventListener('click', onFilterClick);
+}
+
+initializeFilters();
